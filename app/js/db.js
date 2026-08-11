cc9ab2d29206eb6e33252103a3b3dd57cb669422
Shahriyar CSE <shahriyarcse@arch.com>
2026-08-12 01:24:50 +0600

    fix(db): ensure multi-tab sync is initialized on all database resolution paths

--- app/js/db.js
+++ app/js/db.js
@@ -51,6 +51,7 @@ const DB = {
           .then(() => this._migrateFromLocalStorage())
           .then(() => {
             this.migrate();
+            this._setupMultiTabSync();
             safeResolve();
           })
           .catch((err) => {
@@ -65,6 +66,7 @@ const DB = {
         clearTimeout(timeout);
         console.error("IndexedDB onerror, falling back to localStorage", e);
         this._fallbackToLocalStorage();
+        this._setupMultiTabSync();
         safeResolve();
       };
     });
